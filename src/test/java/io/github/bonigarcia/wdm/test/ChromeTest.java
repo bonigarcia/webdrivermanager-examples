1d7b0582dd6701b8abb510bdcacf87ec8c00ab82
Boni Garcia <bgarcia@gsyc.es>
2016-11-16 09:22:08 +0100

    Add test logic to examples (navigate to Wikipedia and so on)

--- src/test/java/io/github/bonigarcia/wdm/test/ChromeTest.java
+++ src/test/java/io/github/bonigarcia/wdm/test/ChromeTest.java
@@ -21,8 +21,11 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import io.github.bonigarcia.wdm.ChromeDriverManager;
 
@@ -55,7 +58,19 @@ public class ChromeTest {
 
   @Test
   public void test() {
-    // Your test code here
+    // Your test code here. For example:
+    WebDriverWait wait = new WebDriverWait(driver, 30); // 30 seconds of timeout
+    driver.get("https://en.wikipedia.org/wiki/Main_Page"); // navigate to Wikipedia
+
+    By searchInput = By.id("searchInput"); // search for "Software"
+    wait.until(ExpectedConditions.presenceOfElementLocated(searchInput));
+    driver.findElement(searchInput).sendKeys("Software");
+    By searchButton = By.id("searchButton");
+    wait.until(ExpectedConditions.elementToBeClickable(searchButton));
+    driver.findElement(searchButton).click();
+
+    wait.until(ExpectedConditions.textToBePresentInElementLocated(By.tagName("body"),
+        "Computer software")); // assert that the resulting page contains a text
   }
 
 }
--- src/test/java/io/github/bonigarcia/wdm/test/MultipleBrowsersTest.java
+++ src/test/java/io/github/bonigarcia/wdm/test/MultipleBrowsersTest.java
@@ -27,9 +27,12 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameter;
 import org.junit.runners.Parameterized.Parameters;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -67,7 +70,19 @@ public class MultipleBrowsersTest {
 
   @Test
   public void test() {
-    // Your test code here
+    // Your test code here. For example:
+    WebDriverWait wait = new WebDriverWait(driver, 30); // 30 seconds of timeout
+    driver.get("https://en.wikipedia.org/wiki/Main_Page"); // navigate to Wikipedia
+
+    By searchInput = By.id("searchInput"); // search for "Software"
+    wait.until(ExpectedConditions.presenceOfElementLocated(searchInput));
+    driver.findElement(searchInput).sendKeys("Software");
+    By searchButton = By.id("searchButton");
+    wait.until(ExpectedConditions.elementToBeClickable(searchButton));
+    driver.findElement(searchButton).click();
+
+    wait.until(ExpectedConditions.textToBePresentInElementLocated(By.tagName("body"),
+        "Computer software")); // assert that the resulting page contains a text
   }
 
 }
