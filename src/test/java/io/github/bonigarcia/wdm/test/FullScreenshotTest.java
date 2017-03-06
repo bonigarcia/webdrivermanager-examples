8fe2f9c081a8f6107891974a33f405b8b9c558cc
Boni Garcia <bgarcia@gsyc.es>
2017-03-06 13:22:03 +0100

    Include snapshots examples

---
+++ src/test/java/io/github/bonigarcia/wdm/test/FullScreenshotTest.java
@@ -0,0 +1,76 @@
+/*
+ * (C) Copyright 2017 Boni Garcia (http://bonigarcia.github.io/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package io.github.bonigarcia.wdm.test;
+
+import java.awt.Rectangle;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+import javax.imageio.ImageIO;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.ChromeDriverManager;
+
+/**
+ * Test with Chrome.
+ *
+ * @author Boni Garcia (boni.gg@gmail.com)
+ * @since 1.0.0
+ */
+public class FullScreenshotTest {
+
+	private WebDriver driver;
+
+	@BeforeClass
+	public static void setupClass() {
+		ChromeDriverManager.getInstance().setup();
+	}
+
+	@Before
+	public void setupTest() {
+		driver = new ChromeDriver();
+	}
+
+	@After
+	public void teardown() {
+		if (driver != null) {
+			driver.quit();
+		}
+	}
+
+	@Test
+	public void test() throws Exception {
+		driver.get("https://en.wikipedia.org/wiki/Main_Page");
+
+		Robot robot = new Robot();
+		Rectangle captureSize = new Rectangle(
+				Toolkit.getDefaultToolkit().getScreenSize());
+		BufferedImage capture = robot.createScreenCapture(captureSize);
+
+		ImageIO.write(capture, "jpg", new File("full-screenshot.jpg"));
+	}
+
+}
---
+++ src/test/java/io/github/bonigarcia/wdm/test/ScreenshotTest.java
@@ -0,0 +1,71 @@
+/*
+ * (C) Copyright 2017 Boni Garcia (http://bonigarcia.github.io/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package io.github.bonigarcia.wdm.test;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.ChromeDriverManager;
+
+/**
+ * Test with Chrome.
+ *
+ * @author Boni Garcia (boni.gg@gmail.com)
+ * @since 1.0.0
+ */
+public class ScreenshotTest {
+
+	private WebDriver driver;
+
+	@BeforeClass
+	public static void setupClass() {
+		ChromeDriverManager.getInstance().setup();
+	}
+
+	@Before
+	public void setupTest() {
+		driver = new ChromeDriver();
+	}
+
+	@After
+	public void teardown() {
+		if (driver != null) {
+			driver.quit();
+		}
+	}
+
+	@Test
+	public void test() throws IOException {
+		driver.get("https://en.wikipedia.org/wiki/Main_Page");
+
+		File scrFile = ((TakesScreenshot) driver)
+				.getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(scrFile, new File(("screenshot.jpg")));
+	}
+
+}
