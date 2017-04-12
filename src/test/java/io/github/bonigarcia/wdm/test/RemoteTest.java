b4a25f2162d6d6d67275b5b7c03c6c71a29844e1
Boni Garcia <bgarcia@gsyc.es>
2017-04-13 00:54:59 +0200

    Include Selenium Grid code examples

--- src/test/java/io/github/bonigarcia/wdm/test/RemoteTest.java
+++ src/test/java/io/github/bonigarcia/wdm/test/RemoteTest.java
@@ -23,12 +23,9 @@ import java.net.URL;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 /**
  * Test with Chrome in Selenium Grid.
@@ -38,35 +35,25 @@ import org.openqa.selenium.support.ui.WebDriverWait;
  */
 public class RemoteTest {
 
-	private WebDriver driver;
+    private WebDriver driver;
 
-	@Before
-	public void setupTest() throws MalformedURLException {
-		DesiredCapabilities capability = DesiredCapabilities.chrome();
-		driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"),
-				capability);
-	}
+    @Before
+    public void setupTest() throws MalformedURLException {
+        DesiredCapabilities capability = DesiredCapabilities.chrome();
+        driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"),
+                capability);
+    }
 
-	@After
-	public void teardown() {
-		if (driver != null) {
-			driver.quit();
-		}
-	}
+    @After
+    public void teardown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
 
-	@Test
-	public void test() {
-		WebDriverWait wait = new WebDriverWait(driver, 30);
-		driver.get("https://en.wikipedia.org/wiki/Main_Page");
-		By searchInput = By.id("searchInput");
-		wait.until(ExpectedConditions.presenceOfElementLocated(searchInput));
-		driver.findElement(searchInput).sendKeys("Software");
-		By searchButton = By.id("searchButton");
-		wait.until(ExpectedConditions.elementToBeClickable(searchButton));
-		driver.findElement(searchButton).click();
-
-		wait.until(ExpectedConditions.textToBePresentInElementLocated(
-				By.tagName("body"), "Computer software"));
-	}
+    @Test
+    public void test() {
+        driver.get("https://en.wikipedia.org/wiki/Main_Page");
+    }
 
 }
