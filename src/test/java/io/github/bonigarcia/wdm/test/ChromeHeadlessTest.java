b435d15148c2a6637b78557e9eb98fc11c87c62b
Boni Garcia <bgarcia@gsyc.es>
2017-06-20 10:59:03 +0200

    Include Chrome headless example

---
+++ src/test/java/io/github/bonigarcia/wdm/test/ChromeHeadlessTest.java
@@ -0,0 +1,68 @@
+/*
+ * (C) Copyright 2017 Boni Garcia (http://bonigarcia.github.io/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package io.github.bonigarcia.wdm.test;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import io.github.bonigarcia.wdm.ChromeDriverManager;
+
+/**
+ * Test with headless Chrome.
+ *
+ * @author Boni Garcia (boni.gg@gmail.com)
+ * @since 1.0.0
+ */
+public class ChromeHeadlessTest {
+
+    private WebDriver driver;
+
+    @BeforeClass
+    public static void setupClass() {
+        ChromeDriverManager.getInstance().setup();
+    }
+
+    @Before
+    public void setupTest() {
+        ChromeOptions options = new ChromeOptions();
+        // Tested in Google Chrome 59 on Linux. More info on:
+        // https://developers.google.com/web/updates/2017/04/headless-chrome
+        options.addArguments("--headless");
+        options.addArguments("--disable-gpu");
+        driver = new ChromeDriver(options);
+    }
+
+    @After
+    public void teardown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    @Test
+    public void test() {
+        driver.get("https://en.wikipedia.org/wiki/Main_Page");
+        String title = driver.getTitle();
+        System.out.println(title);
+    }
+
+}
