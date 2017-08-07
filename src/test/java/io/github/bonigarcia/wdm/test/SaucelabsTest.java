3d4412a06e382e9dc807b9d08e3f87f8b8097088
Boni Garcia <bgarcia@gsyc.es>
2017-08-07 08:16:45 +0200

    Add example using Saucelabs

---
+++ src/test/java/io/github/bonigarcia/wdm/test/SaucelabsTest.java
@@ -0,0 +1,70 @@
+/*
+ * (C) Copyright 2017 Boni Garcia (http://bonigarcia.github.io/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package io.github.bonigarcia.wdm.test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+/**
+ * Test with Saucelabs.
+ *
+ * @author Boni Garcia (boni.gg@gmail.com)
+ * @since 1.0.0
+ */
+public class SaucelabsTest {
+
+    private WebDriver driver;
+
+    public static final String USERNAME = "YOUR_USERNAME";
+    public static final String ACCESS_KEY = "YOUR_ACCESS_KEY";
+    public static final String URL = "https://" + USERNAME + ":" + ACCESS_KEY
+            + "@ondemand.saucelabs.com:443/wd/hub";
+
+    @Before
+    public void setupTest() throws MalformedURLException {
+        DesiredCapabilities caps = DesiredCapabilities.chrome();
+        caps.setCapability("platform", "Windows 10");
+        caps.setCapability("version", "52.0");
+
+        driver = new RemoteWebDriver(new URL(URL), caps);
+    }
+
+    @After
+    public void teardown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    @Test
+    public void test() throws InterruptedException {
+        driver.get("https://en.wikipedia.org/wiki/Main_Page");
+        String title = driver.getTitle();
+        System.out.println(title);
+
+        Thread.sleep(30000); // Wait to see the page
+    }
+
+}
