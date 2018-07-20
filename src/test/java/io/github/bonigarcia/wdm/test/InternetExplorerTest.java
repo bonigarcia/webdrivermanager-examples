f85c0c9d2645ac3fec9a302b8bdc18fe6702a70d
Boni Garcia <bgarcia@gsyc.es>
2018-07-20 17:09:00 +0200

    Simplify Internet Explorer test

--- src/test/java/io/github/bonigarcia/wdm/test/InternetExplorerTest.java
+++ src/test/java/io/github/bonigarcia/wdm/test/InternetExplorerTest.java
@@ -17,20 +17,16 @@
 
 package io.github.bonigarcia.wdm.test;
 
-import static org.openqa.selenium.support.ui.ExpectedConditions.elementToBeClickable;
-import static org.openqa.selenium.support.ui.ExpectedConditions.presenceOfElementLocated;
-import static org.openqa.selenium.support.ui.ExpectedConditions.textToBePresentInElementLocated;
+import static org.junit.Assert.assertNotNull;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
-import io.github.bonigarcia.wdm.InternetExplorerDriverManager;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 /**
  * Test with Internet Explorer.
@@ -44,7 +40,7 @@ public class InternetExplorerTest {
 
     @BeforeClass
     public static void setupClass() {
-        InternetExplorerDriverManager.getInstance().setup();
+        WebDriverManager.iedriver().setup();
     }
 
     @Before
@@ -61,19 +57,8 @@ public class InternetExplorerTest {
 
     @Test
     public void test() {
-        // Your test code here. For example:
-        WebDriverWait wait = new WebDriverWait(driver, 30);
-        driver.get("https://en.wikipedia.org/wiki/Main_Page");
-
-        By searchInput = By.id("searchInput");
-        wait.until(presenceOfElementLocated(searchInput));
-        driver.findElement(searchInput).sendKeys("Software");
-        By searchButton = By.id("searchButton");
-        wait.until(elementToBeClickable(searchButton));
-        driver.findElement(searchButton).click();
-
-        wait.until(textToBePresentInElementLocated(By.tagName("body"),
-                "Computer software"));
+        assertNotNull(driver);
+        driver.get("https://bonigarcia.github.io/selenium-jupiter/");
     }
 
 }
