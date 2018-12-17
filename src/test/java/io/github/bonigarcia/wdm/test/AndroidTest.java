8a83e2e2a518d9aa816b01cb1a7cc4d9c021fbde
Boni Garcia <bgarcia@gsyc.es>
2018-12-17 12:20:24 +0100

    Include test for Android device using RemoteWebDriver

---
+++ src/test/java/io/github/bonigarcia/wdm/test/AndroidTest.java
@@ -0,0 +1,65 @@
+/*
+ * (C) Copyright 2018 Boni Garcia (http://bonigarcia.github.io/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package io.github.bonigarcia.wdm.test;
+
+import static org.junit.Assert.assertTrue;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+/**
+ * Test with Android device.
+ *
+ * @author Boni Garcia (boni.gg@gmail.com)
+ * @since 1.0.0
+ */
+public class AndroidTest {
+
+    private WebDriver driver;
+
+    @Before
+    public void setupTest() throws MalformedURLException {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("platformName", "android");
+        capabilities.setCapability("browserName", "browser");
+        capabilities.setCapability("deviceName", "Samsung Galaxy S6");
+        driver = new RemoteWebDriver(new URL("http://172.17.0.1:4444/wd/hub"),
+                capabilities);
+    }
+
+    @After
+    public void teardown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    @Test
+    public void test() {
+        driver.get("https://en.wikipedia.org/wiki/Main_Page");
+        assertTrue(
+                driver.getTitle().equals("Wikipedia, the free encyclopedia"));
+    }
+
+}
