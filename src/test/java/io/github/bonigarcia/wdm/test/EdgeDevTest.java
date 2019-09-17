6c4cf1daef7cae4c91982b85aaf1bef007584e66
Boni Garcia <bgarcia@gsyc.es>
2019-09-17 09:40:37 +0200

    Include Edge in development version (Chromium based) test

---
+++ src/test/java/io/github/bonigarcia/wdm/test/EdgeDevTest.java
@@ -0,0 +1,71 @@
+/*
+ * (C) Copyright 2019 Boni Garcia (http://bonigarcia.github.io/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package io.github.bonigarcia.wdm.test;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+/**
+ * Test with Edge Dev.
+ *
+ * @author Boni Garcia (boni.gg@gmail.com)
+ * @since 1.0.0
+ */
+public class EdgeDevTest {
+
+    private WebDriver driver;
+
+    @BeforeClass
+    public static void setupClass() {
+        WebDriverManager.edgedriver().setup();
+    }
+
+    @Before
+    public void setupTest() {
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.setBinary(
+                "C:\\Program Files (x86)\\Microsoft\\Edge Dev\\Application\\msedge.exe");
+        EdgeOptions edgeOptions = new EdgeOptions().merge(chromeOptions);
+        driver = new EdgeDriver(edgeOptions);
+    }
+
+    @After
+    public void teardown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    @Test
+    public void test() {
+        driver.get("https://bonigarcia.github.io/selenium-jupiter/");
+        assertThat(driver.getTitle(),
+                containsString("JUnit 5 extension for Selenium"));
+    }
+
+}
