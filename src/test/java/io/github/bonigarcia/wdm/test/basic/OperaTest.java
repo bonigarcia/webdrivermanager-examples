771013b252d3b7c103de9f48185a152915d48aad
Boni Garcia <boni.garcia@uc3m.es>
2022-03-28 18:12:43 +0200

    Include Opera test using Selenium 4.1.3 and WebDriverManager 5.1.0

---
+++ src/test/java/io/github/bonigarcia/wdm/test/basic/OperaTest.java
@@ -0,0 +1,69 @@
+/*
+ * (C) Copyright 2022 Boni Garcia (https://bonigarcia.github.io/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package io.github.bonigarcia.wdm.test.basic;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assumptions.assumeThat;
+
+import java.nio.file.Path;
+import java.util.Optional;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+class OperaTest {
+
+    WebDriver driver;
+    static Optional<Path> browserPath;
+
+    @BeforeAll
+    static void setupClass() {
+        WebDriverManager operadriver = WebDriverManager.operadriver();
+        browserPath = operadriver.getBrowserPath();
+        assumeThat(browserPath.isPresent());
+        operadriver.setup();
+        System.setProperty("webdriver.chrome.driver",
+                operadriver.getDownloadedDriverPath());
+    }
+
+    @BeforeEach
+    void setupTest() {
+        ChromeOptions options = new ChromeOptions();
+        options.setBinary(browserPath.get().toFile());
+        driver = new ChromeDriver();
+    }
+
+    @AfterEach
+    void teardown() {
+        driver.quit();
+    }
+
+    @Test
+    void test() {
+        driver.get("https://bonigarcia.dev/selenium-webdriver-java/");
+        assertThat(driver.getTitle())
+                .isEqualTo("Hands-On Selenium WebDriver with Java");
+    }
+
+}
